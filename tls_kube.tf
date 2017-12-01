22ec1a0fa96703bb0099c9aa053979a94d63f09d
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2017-12-01 06:28:11 +0100

    api: add proxy_client support

--- tls_kube.tf
+++ tls_kube.tf
@@ -22,12 +22,12 @@ resource "tls_self_signed_cert" "kube_ca" {
   ]
 }
 
+// admin cert stored in kubeconfig
+
 resource "tls_private_key" "admin" {
   algorithm = "RSA"
   rsa_bits  = "2048"
 }
-
-// admin cert stored in kubeconfig
 
 resource "tls_cert_request" "admin" {
   key_algorithm   = "${tls_private_key.admin.algorithm}"
@@ -64,4 +64,37 @@ resource "tls_private_key" "service_account" {
 resource "tls_private_key" "kubelet" {
   algorithm = "RSA"
   rsa_bits  = "2048"
+}
+
+// proxy client cert
+
+resource "tls_private_key" "proxy_client" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "proxy_client" {
+  key_algorithm   = "${tls_private_key.proxy_client.algorithm}"
+  private_key_pem = "${tls_private_key.proxy_client.private_key_pem}"
+
+  subject {
+    common_name  = "kube"
+    organization = "proxy_client"
+  }
+}
+
+resource "tls_locally_signed_cert" "proxy_client" {
+  cert_request_pem = "${tls_cert_request.proxy_client.cert_request_pem}"
+
+  ca_key_algorithm      = "${tls_self_signed_cert.kube_ca.key_algorithm}"
+  ca_private_key_pem    = "${tls_private_key.kube_ca.private_key_pem}"
+  ca_cert_pem           = "${tls_self_signed_cert.kube_ca.cert_pem}"
+  validity_period_hours = "${var.validity_period}"
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "client_auth",
+    "server_auth",
+  ]
 }
